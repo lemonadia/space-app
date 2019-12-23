cf8c36cf4e0b8b03c19f37d621ec77112f3e6b3f
Nadia TUmidajska <nadia.tumidajska@gmail.com>
2019-12-23 17:10:11 +0100

    Add simple styles to search input

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,6 +1,8 @@
+
 <template>
-  <div class="home">
-    <h1>spacer</h1>
+  <div class="wrapper">
+    <div class="search"><label for="name">Search</label>
+      <input name="search" /></div>
   </div>
 </template>
 
@@ -9,3 +11,29 @@ export default {
   name: 'Home',
 };
 </script>
+
+<style lang="scss" scoped>
+.wrapper{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  padding: 30px;
+  margin: 0;
+   }
+  .search{
+    display: flex;
+    flex-direction:column;
+    width: 300px;
+  }
+  label{
+    font-family: Montserrot, sans-serif;
+  }
+  input{
+    height: 30px;
+    border: 0;
+    border-bottom: 1px solid black;
+  }
+
+
+</style>
