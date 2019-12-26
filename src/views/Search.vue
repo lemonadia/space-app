d625bbee58cf84e9dbe091ff11d7966642e6f8c4
Nadia TUmidajska <nadia.tumidajska@gmail.com>
2019-12-26 18:36:10 +0100

    Add API to the project

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,14 +1,38 @@
 
 <template>
   <div class="wrapper">
-    <div class="search"><label for="name">Search</label>
-      <input name="search" /></div>
+    <div class="search">
+      <label for="name">Search</label>
+      <input name="search" id="search" v-model="searchValue" v-on:input="handleInput" />
+    </div>
   </div>
 </template>
 
 <script>
+
+import axios from 'axios';
+import debounce from 'lodash.debounce';
+
+const API = 'https://images-api.nasa.gov/search';
+
 export default {
-  name: 'Home',
+  name: 'Search',
+  data() {
+    return {
+      searchValue: '',
+    };
+  },
+  methods: {
+    handleInput: debounce(function () {
+      axios.get(`${API}?q=${this.searchValue}&media_type=image`)
+        .then((response) => {
+          console.log(response.data.collection.items);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }, 500),
+  },
 };
 </script>
 
